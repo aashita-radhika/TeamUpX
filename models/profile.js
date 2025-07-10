4a6b57efbb8912a70407c0a5316961f6e0495800
Aashita Radhika <164779243+aashita-radhika@users.noreply.github.com>
2025-07-10 23:25:34 +0530

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,23 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const UserSchema = new mongoose.Schema({
-//     username:   { type: String, required: true },
-//     email:      { type: String, required: true, unique: true },
-//     password:   { type: String, required: true },
-//     bio:        { type: String },
-//     fullName:   { type: String, required: true },
-//     college:    { type: String },
-//     yearOfStudy:{ type: String }, 
-//     skills:     [String], 
-//     pastExperience: { type: String }, 
-//     achievements:   { type: String }, 
-//     resume:     { type: String },
-//     github:     { type: String }
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
-
-
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
